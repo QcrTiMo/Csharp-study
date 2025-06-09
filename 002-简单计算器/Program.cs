7be84ef53d522bbc331d9cd9984f90569efbcc08
QcrTiMo <3192377376@qq.com>
2025-06-10 00:09:27 +0800

    update

---
+++ 002-简单计算器/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _002_简单计算器
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
