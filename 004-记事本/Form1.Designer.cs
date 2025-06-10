ef55ba1fdf822a24dc307a9d3329ecc52faf6f37
QcrTiMo <3192377376@qq.com>
2025-06-10 22:08:45 +0800

    update

--- 004-记事本/Form1.Designer.cs
+++ 004-记事本/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _004_记事本
 {
-    partial class Form1
+    partial class mainview
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,325 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menu = new MenuStrip();
+            menuFile = new ToolStripMenuItem();
+            menuFileNew = new ToolStripMenuItem();
+            menuFileNewView = new ToolStripMenuItem();
+            menuFileOpen = new ToolStripMenuItem();
+            menuFileSave = new ToolStripMenuItem();
+            menuFileSaveAs = new ToolStripMenuItem();
+            menuFileSetting = new ToolStripMenuItem();
+            menuFilePrint = new ToolStripMenuItem();
+            menuFileExit = new ToolStripMenuItem();
+            menuFileEdit = new ToolStripMenuItem();
+            menuFileZ = new ToolStripMenuItem();
+            menuFileX = new ToolStripMenuItem();
+            menuFileC = new ToolStripMenuItem();
+            menuFileV = new ToolStripMenuItem();
+            menuFileD = new ToolStripMenuItem();
+            menuFileF = new ToolStripMenuItem();
+            menuFileF3 = new ToolStripMenuItem();
+            menuFileSF3 = new ToolStripMenuItem();
+            menuFileH = new ToolStripMenuItem();
+            menuFileG = new ToolStripMenuItem();
+            menuFileA = new ToolStripMenuItem();
+            menuFileF5 = new ToolStripMenuItem();
+            menuFormatFont = new ToolStripMenuItem();
+            menuFileCheck = new ToolStripMenuItem();
+            zoom = new ToolStripMenuItem();
+            large = new ToolStripMenuItem();
+            small = new ToolStripMenuItem();
+            back_to_default = new ToolStripMenuItem();
+            status_bar = new ToolStripMenuItem();
+            AutoLM = new ToolStripMenuItem();
+            txtEditor = new TextBox();
+            menu.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // menu
+            // 
+            menu.ImageScalingSize = new Size(20, 20);
+            menu.Items.AddRange(new ToolStripItem[] { menuFile, menuFileEdit, menuFileCheck });
+            menu.Location = new Point(0, 0);
+            menu.Name = "menu";
+            menu.Size = new Size(907, 28);
+            menu.TabIndex = 0;
+            menu.Text = "menuStrip1";
+            menu.ItemClicked += menu_ItemClicked;
+            // 
+            // menuFile
+            // 
+            menuFile.DropDownItems.AddRange(new ToolStripItem[] { menuFileNew, menuFileNewView, menuFileOpen, menuFileSave, menuFileSaveAs, menuFileSetting, menuFilePrint, menuFileExit });
+            menuFile.Name = "menuFile";
+            menuFile.Size = new Size(53, 24);
+            menuFile.Text = "文件";
+            menuFile.Click += menuFile_Click;
+            // 
+            // menuFileNew
+            // 
+            menuFileNew.Name = "menuFileNew";
+            menuFileNew.Size = new Size(265, 26);
+            menuFileNew.Text = "新建            Ctrl+N";
+            menuFileNew.Click += menuFileNew_Click;
+            // 
+            // menuFileNewView
+            // 
+            menuFileNewView.Name = "menuFileNewView";
+            menuFileNewView.Size = new Size(265, 26);
+            menuFileNewView.Text = "新建窗口     Ctrl+Shift+N";
+            menuFileNewView.Click += menuFileNewView_Click;
+            // 
+            // menuFileOpen
+            // 
+            menuFileOpen.Name = "menuFileOpen";
+            menuFileOpen.Size = new Size(265, 26);
+            menuFileOpen.Text = "打开             Ctrl+O";
+            menuFileOpen.Click += menuFileOpen_Click;
+            // 
+            // menuFileSave
+            // 
+            menuFileSave.Name = "menuFileSave";
+            menuFileSave.Size = new Size(265, 26);
+            menuFileSave.Text = "保存             Ctrl+S";
+            menuFileSave.Click += menuFileSave_Click;
+            // 
+            // menuFileSaveAs
+            // 
+            menuFileSaveAs.Name = "menuFileSaveAs";
+            menuFileSaveAs.Size = new Size(265, 26);
+            menuFileSaveAs.Text = "另存为         Ctrl+Shift+S";
+            menuFileSaveAs.Click += menuFileSaveAs_Click;
+            // 
+            // menuFileSetting
+            // 
+            menuFileSetting.Name = "menuFileSetting";
+            menuFileSetting.Size = new Size(265, 26);
+            menuFileSetting.Text = "页面设置";
+            menuFileSetting.Click += menuFileSetting_Click;
+            // 
+            // menuFilePrint
+            // 
+            menuFilePrint.Name = "menuFilePrint";
+            menuFilePrint.Size = new Size(265, 26);
+            menuFilePrint.Text = "打印             Ctrl+P";
+            menuFilePrint.Click += menuFilePrint_Click;
+            // 
+            // menuFileExit
+            // 
+            menuFileExit.Name = "menuFileExit";
+            menuFileExit.Size = new Size(265, 26);
+            menuFileExit.Text = "退出";
+            menuFileExit.Click += 退出ToolStripMenuItem_Click;
+            // 
+            // menuFileEdit
+            // 
+            menuFileEdit.DropDownItems.AddRange(new ToolStripItem[] { menuFileZ, menuFileX, menuFileC, menuFileV, menuFileD, menuFileF, menuFileF3, menuFileSF3, menuFileH, menuFileG, menuFileA, menuFileF5, menuFormatFont });
+            menuFileEdit.Name = "menuFileEdit";
+            menuFileEdit.Size = new Size(53, 24);
+            menuFileEdit.Text = "编辑";
+            menuFileEdit.Click += menuFileEdit_Click;
+            // 
+            // menuFileZ
+            // 
+            menuFileZ.Name = "menuFileZ";
+            menuFileZ.Size = new Size(260, 26);
+            menuFileZ.Text = "撤销                    Ctrl+Z";
+            menuFileZ.Click += 撤销ToolStripMenuItem_Click;
+            // 
+            // menuFileX
+            // 
+            menuFileX.Name = "menuFileX";
+            menuFileX.Size = new Size(260, 26);
+            menuFileX.Text = "剪切                    Ctrl+X";
+            menuFileX.Click += menuFileX_Click;
+            // 
+            // menuFileC
+            // 
+            menuFileC.Name = "menuFileC";
+            menuFileC.Size = new Size(260, 26);
+            menuFileC.Text = "复制                    Ctrl+C";
+            menuFileC.Click += menuFileC_Click;
+            // 
+            // menuFileV
+            // 
+            menuFileV.Name = "menuFileV";
+            menuFileV.Size = new Size(260, 26);
+            menuFileV.Text = "黏贴                    Ctrl+V";
+            menuFileV.Click += menuFileV_Click;
+            // 
+            // menuFileD
+            // 
+            menuFileD.Name = "menuFileD";
+            menuFileD.Size = new Size(260, 26);
+            menuFileD.Text = "删除                    Del";
+            menuFileD.Click += menuFileD_Click;
+            // 
+            // menuFileF
+            // 
+            menuFileF.Name = "menuFileF";
+            menuFileF.Size = new Size(260, 26);
+            menuFileF.Text = "查找                    Ctrl+F";
+            menuFileF.Click += menuFileF_Click;
+            // 
+            // menuFileF3
+            // 
+            menuFileF3.Name = "menuFileF3";
+            menuFileF3.Size = new Size(260, 26);
+            menuFileF3.Text = "查找下一个         F3";
+            menuFileF3.Click += menuFileF3_Click;
+            // 
+            // menuFileSF3
+            // 
+            menuFileSF3.Name = "menuFileSF3";
+            menuFileSF3.Size = new Size(260, 26);
+            menuFileSF3.Text = "查找上一个        Shift+F3";
+            menuFileSF3.Click += menuFileSF3_Click;
+            // 
+            // menuFileH
+            // 
+            menuFileH.Name = "menuFileH";
+            menuFileH.Size = new Size(260, 26);
+            menuFileH.Text = "替换                    Ctrl+H";
+            menuFileH.Click += menuFileH_Click;
+            // 
+            // menuFileG
+            // 
+            menuFileG.Name = "menuFileG";
+            menuFileG.Size = new Size(260, 26);
+            menuFileG.Text = "转到                    Ctrl+G";
+            menuFileG.Click += menuFileG_Click;
+            // 
+            // menuFileA
+            // 
+            menuFileA.Name = "menuFileA";
+            menuFileA.Size = new Size(260, 26);
+            menuFileA.Text = "全选                    Ctrl+A";
+            menuFileA.Click += menuFileA_Click;
+            // 
+            // menuFileF5
+            // 
+            menuFileF5.Name = "menuFileF5";
+            menuFileF5.Size = new Size(260, 26);
+            menuFileF5.Text = "时间/日期           F5";
+            menuFileF5.Click += menuFileF5_Click;
+            // 
+            // menuFormatFont
+            // 
+            menuFormatFont.Name = "menuFormatFont";
+            menuFormatFont.Size = new Size(260, 26);
+            menuFormatFont.Text = "字体";
+            menuFormatFont.Click += menuFormatFont_Click;
+            // 
+            // menuFileCheck
+            // 
+            menuFileCheck.DropDownItems.AddRange(new ToolStripItem[] { zoom, status_bar, AutoLM });
+            menuFileCheck.Name = "menuFileCheck";
+            menuFileCheck.Size = new Size(53, 24);
+            menuFileCheck.Text = "查看";
+            menuFileCheck.Click += menuFileCheck_Click;
+            // 
+            // zoom
+            // 
+            zoom.DropDownItems.AddRange(new ToolStripItem[] { large, small, back_to_default });
+            zoom.Name = "zoom";
+            zoom.Size = new Size(152, 26);
+            zoom.Text = "缩放";
+            zoom.Click += zoom_Click;
+            // 
+            // large
+            // 
+            large.Name = "large";
+            large.Size = new Size(281, 26);
+            large.Text = "放大                       Ctrl+加号";
+            large.Click += large_Click;
+            // 
+            // small
+            // 
+            small.Name = "small";
+            small.Size = new Size(281, 26);
+            small.Text = "缩小                       Ctrl+减号";
+            small.Click += 缩小ToolStripMenuItem_Click;
+            // 
+            // back_to_default
+            // 
+            back_to_default.Name = "back_to_default";
+            back_to_default.Size = new Size(281, 26);
+            back_to_default.Text = "还原默认缩放        Ctrl+0";
+            back_to_default.Click += back_to_default_Click;
+            // 
+            // status_bar
+            // 
+            status_bar.Name = "status_bar";
+            status_bar.Size = new Size(152, 26);
+            status_bar.Text = "状态栏";
+            status_bar.Click += status_bar_Click;
+            // 
+            // AutoLM
+            // 
+            AutoLM.Name = "AutoLM";
+            AutoLM.Size = new Size(152, 26);
+            AutoLM.Text = "自动换行";
+            AutoLM.Click += AutoLM_Click;
+            // 
+            // txtEditor
+            // 
+            txtEditor.Dock = DockStyle.Fill;
+            txtEditor.Location = new Point(0, 28);
+            txtEditor.Multiline = true;
+            txtEditor.Name = "txtEditor";
+            txtEditor.ScrollBars = ScrollBars.Both;
+            txtEditor.Size = new Size(907, 581);
+            txtEditor.TabIndex = 1;
+            // 
+            // mainview
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1109, 645);
-            Name = "Form1";
-            Text = "Form1";
+            ClientSize = new Size(907, 609);
+            Controls.Add(txtEditor);
+            Controls.Add(menu);
+            MainMenuStrip = menu;
+            Name = "mainview";
+            Text = "简单记事本";
+            menu.ResumeLayout(false);
+            menu.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menu;
+        private ToolStripMenuItem menuFile;
+        private ToolStripMenuItem menuFileNew;
+        private ToolStripMenuItem menuFileNewView;
+        private ToolStripMenuItem menuFileOpen;
+        private ToolStripMenuItem menuFileSave;
+        private ToolStripMenuItem menuFileSaveAs;
+        private ToolStripMenuItem menuFileSetting;
+        private ToolStripMenuItem menuFilePrint;
+        private ToolStripMenuItem menuFileExit;
+        private ToolStripMenuItem menuFileEdit;
+        private ToolStripMenuItem menuFileCheck;
+        private ToolStripMenuItem zoom;
+        private ToolStripMenuItem large;
+        private ToolStripMenuItem small;
+        private ToolStripMenuItem back_to_default;
+        private ToolStripMenuItem status_bar;
+        private ToolStripMenuItem AutoLM;
+        private ToolStripMenuItem menuFileZ;
+        private ToolStripMenuItem menuFileX;
+        private ToolStripMenuItem menuFileC;
+        private ToolStripMenuItem menuFileV;
+        private ToolStripMenuItem menuFileD;
+        private ToolStripMenuItem menuFileF;
+        private ToolStripMenuItem menuFileF3;
+        private ToolStripMenuItem menuFileSF3;
+        private ToolStripMenuItem menuFileH;
+        private ToolStripMenuItem menuFileG;
+        private ToolStripMenuItem menuFileA;
+        private ToolStripMenuItem menuFileF5;
+        private ToolStripMenuItem menuFormatFont;
+        private TextBox txtEditor;
     }
 }
--- 004-记事本/Form1.cs
+++ 004-记事本/Form1.cs
@@ -1,10 +1,308 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
 namespace _004_记事本
 {
-    public partial class Form1 : Form
+    public partial class mainview : Form
     {
-        public Form1()
+        private String currentFilePath = null;  //用于存储当前打开文件路径
+        private bool textChanged = false;  //用于标记文本是否被修改过
+        public mainview()
         {
+            //可以在这里初始化，比如窗口标题
             InitializeComponent();
+            UpdateTitle();
+            //监听文本框内容变化
+            txtEditor.TextChanged += TxtEditor_TextChanged;
+        }
+
+        private void TxtEditor_TextChanged(object sender, EventArgs e)
+        {
+            textChanged = true;  //当文本框内容变化时，设置标记为true
+            UpdateTitle();  //更新窗口标题
+        }
+
+        private void UpdateTitle()
+        {
+            String fileName = String.IsNullOrEmpty(currentFilePath) ? "无标题" : Path.GetFileName(currentFilePath);
+            this.Text = fileName + (textChanged ? "*" : "") + "- 记事本";
+        }
+
+        private void 撤销ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtEditor.CanUndo)
+            {
+                txtEditor.Undo();
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 缩小ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void status_bar_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void AutoLM_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileNew_Click(object sender, EventArgs e)
+        {
+            if (SaveChangesIfNecessary() == DialogResult.Cancel)
+            {
+                return;  //如果用户选择取消，则不执行新建操作
+            }
+            txtEditor.Clear();  //清空文本框内容
+            currentFilePath = null;  //重置当前文件路径
+            textChanged = false;  //重置文本修改标记
+            UpdateTitle();  //更新窗口标题
+        }
+
+        private void menuFileNewView_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileOpen_Click(object sender, EventArgs e)
+        {
+            if (SaveChangesIfNecessary() == DialogResult.Cancel)
+            {
+                return;  //如果用户选择取消，则不执行新建操作
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "文本文档 (*.txt)|*.txt|所有文件 (*.*)|*.*";  //设置文件类型过滤器
+            openFileDialog.Title = "打开文件";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    currentFilePath = openFileDialog.FileName;
+                    txtEditor.Text = File.ReadAllText(currentFilePath);  //读取文件内容到文本框
+                    textChanged = false;  //重置文本修改标记
+                    UpdateTitle();  //更新窗口标题
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("打开文件失败: " + ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool SaveFile()
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                return SaveFileAs(); //如果没有路径，则调用另存为
+            }
+            else
+            {
+                try
+                {
+                    File.WriteAllText(currentFilePath, txtEditor.Text);
+                    textChanged = false;
+                    UpdateTitle();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("保存文件失败: " + ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+        }
+
+        private bool SaveFileAs()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "文本文档 (*.txt)|*.txt|所有文件 (*.*)|*.*";
+            saveFileDialog.Title = "另存为";
+            saveFileDialog.DefaultExt = "txt";  //默认扩展名
+            saveFileDialog.AddExtension = true;  //自动添加扩展名
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = saveFileDialog.FileName;
+                return SaveFile();  //调用 SaveFile 来实际保存
+            }
+            return false;  //用户取消了另存为
+        }
+
+        private void menuFileSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void menuFileSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileAs();
+        }
+
+        private DialogResult SaveChangesIfNecessary()
+        {
+            if (textChanged)
+            {
+                DialogResult result = MessageBox.Show(
+                    $"是否要将更改保存到 {(string.IsNullOrEmpty(currentFilePath) ? "无标题" : Path.GetFileName(currentFilePath))}?",
+                    "简单记事本",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    if (!SaveFile())  //如果保存失败 
+                    {
+                        return DialogResult.Cancel;  //阻止后续操作
+                    }
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return DialogResult.Cancel;
+                }
+                //如果是 No，则不保存，继续执行后续操作
+            }
+            return DialogResult.OK;  //表示操作可以继续 (要么没改，要么已保存/不保存)
+        }
+
+        private void menuFileSetting_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFilePrint_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFile_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileEdit_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileX_Click(object sender, EventArgs e)
+        {
+            if (txtEditor.SelectionLength > 0)
+            {
+                txtEditor.Cut();
+            }
+        }
+
+        private void menuFileC_Click(object sender, EventArgs e)
+        {
+            if (txtEditor.SelectionLength > 0)
+            {
+                txtEditor.Copy();
+            }
+        }
+
+        private void menuFileV_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                txtEditor.Paste();
+            }
+        }
+
+        private void menuFileD_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileF_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileF3_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileSF3_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileG_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileA_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileF5_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFormatFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.Font = txtEditor.Font;  //将当前字体设为对话框的默认字体
+            fontDialog.ShowColor = true; 
+
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtEditor.Font = fontDialog.Font;
+            }
+        }
+
+        private void menuFileCheck_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void zoom_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void large_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void back_to_default_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void menuFileH_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void mainview_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (SaveChangesIfNecessary() == DialogResult.Cancel)
+            {
+                e.Cancel = true;  //取消关闭操作
+            }
         }
     }
 }
